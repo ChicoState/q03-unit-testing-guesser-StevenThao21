90b3988ac758581e53554bb05a15bc7b4b9c959f
Steven Thao <sthao21@csuchico.edu>
2024-05-06 02:35:49 -0700

    Checking

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -1,73 +1,22 @@
 /**
  * Unit Tests for the class
- **/
+**/
 
 #include <gtest/gtest.h>
 #include "Guesser.h"
 
 class GuesserTest : public ::testing::Test
 {
-protected:
-	GuesserTest() {}		   // constructor runs before each test
-	virtual ~GuesserTest() {}  // destructor cleans up after tests
-	virtual void SetUp() {}	   // sets up before each test (after constructor)
-	virtual void TearDown() {} // clean up after each test, (before destructor)
+	protected:
+		GuesserTest(){} //constructor runs before each test
+		virtual ~GuesserTest(){} //destructor cleans up after tests
+		virtual void SetUp(){} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 /* Example test
 TEST(GuesserTest, smoke_test)
 {
-	ASSERT_EQ( 1+1, 2 );
+    ASSERT_EQ( 1+1, 2 );
 }
 */
-
-TEST(GuesserTest, Empty_string)
-{
-	Guesser my_guesser("");
-	bool actual = my_guesser.match("");
-	ASSERT_FALSE(actual);
-}
-
-TEST(GuesserTest, matching_test)
-{
-	Guesser my_guesser("Secret Word");
-	int actual = my_guesser.match("");
-	ASSERT_EQ(0, actual);
-}
-
-TEST(GuesserTest, Matching_test_Correct)
-{
-	Guesser my_guesser("Secret");
-	int actual = my_guesser.match("Secret");
-	ASSERT_EQ(1, actual);
-}
-
-TEST(GuesserTest, Empty_string_test)
-{
-	Guesser my_guesser("");
-	ASSERT_EQ(3, my_guesser.remaining());
-}
-
-TEST(GuesserTest, remaining_test)
-{
-	Guesser my_guesser("Secret Word");
-	my_guesser.match("First");
-	my_guesser.match("Second");
-	my_guesser.match("Third");
-	ASSERT_EQ(0, my_guesser.remaining());
-}
-
-TEST(GuesserTest, Matching_case_sen)
-{
-	Guesser my_guesser("Secret");
-	int actual = my_guesser.match("secret");
-	ASSERT_EQ(0, actual);
-}
-
-// In order for me to test the distance I have to move the distance from private to public in the .h file
-// TEST(GuesserTest, distance_string)
-// {
-// 	Guesser my_guesser("Secret Word");
-// 	int actual = my_guesser.distance("Secret");
-// 	ASSERT_EQ(3, actual);
-// }
